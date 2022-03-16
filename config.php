ce01e2146cf3db2addd52d60bd3c9612dbe34b4a
Leon <101542033+hzl0898@users.noreply.github.com>
2022-03-16 16:14:26 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+	//配置信息
+	$CONFIG = array(
+		'db_host'=>"127.0.0.1",	//数据库地址
+		'db_name'=>"naicha",		//数据库名成
+		'db_user'=>"root",		//数据库用户名
+		'db_pass'=>"hzlhzl",		//数据库密码
+		'url'=>"",			//网站根目录地址
+		'webname'=>"奶茶销售网站",	//网站名称
+
+	);
+?>
